7f11778304331ca1209124748a79603ffd84ab50
johannessiedersberger <johannes.siedersberger@gmail.com>
2019-06-28 21:24:23 +0200

    Knight moves

--- ChessGame/ChessGame/Chess.py
+++ ChessGame/ChessGame/Chess.py
@@ -161,8 +161,11 @@ class Bishop(Piece):
       return moves
 
 class Knight(Piece):
+    def knightList(x,y,int1,int2):
+      return [(x+int1,y+int2),(x-int1,y+int2),(x+int1,y-int2),(x-int1,y-int2),(x+int2,y+int1),(x-int2,y+int1),(x+int2,y-int1),(x-int2,y-int1)]
+
     def available_moves(self, x, y):
-      moves = []
+      moves = [(xx,yy) for xx,yy in knightList(x,y,2,1) if self.noConflict(gameboard, Color, xx, yy)]
       return moves
 
     
